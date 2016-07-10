4d3bcd45c14ea027ff3571714538a8462195c374
oden <iamoden41@gmail.com>
2016-07-10 23:26:42 +0900

    10回計算して平均計測時間，経路を表示するMainメソッド(実行結果をdataファイルに)

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.ArrayList;
+
 import data.CitiesData;
 import localsearch.Opt_2;
 import path.TravelingPath;
@@ -11,19 +13,37 @@ public class Main {
 	}
 
 	public static void main(String[] args) {
-		int noOfCities = 100000;
-		CitiesData data = new CitiesData("mona-lisa100K", noOfCities);
-		TravelingPath path = new TravelingPath(noOfCities);
-		path.init();
-		Opt_2 opt2 = new Opt_2(noOfCities, path);
-		long start = System.currentTimeMillis();
+		int noOfCities = 71009;
+		ArrayList<Long> timeList = new ArrayList<>();
+		ArrayList<Double> pathLengthList = new ArrayList<>();
 
-		opt2.doOneIterator();
+		for (int i = 0; i < 11; i++) {
+		    CitiesData data = new CitiesData("ch71009", noOfCities);
+		    TravelingPath path = new TravelingPath(noOfCities);
+		    path.init();
+		    Opt_2 opt2 = new Opt_2(noOfCities, path);
+		    long start = System.currentTimeMillis();
 
-		long end = System.currentTimeMillis();
+		    opt2.doOneIterator();
 
-		System.out.println((end - start)  + "ms");
-		System.out.println(path.getTourLength());
+		    long end = System.currentTimeMillis();
+
+		    System.out.println((end - start)  + "ms");
+		    System.out.println(path.getTourLength());
+
+		    if(i != 0){
+			timeList.add(end - start);
+			pathLengthList.add(path.getTourLength());
+		    }
+		}
+
+		System.out.println("-------------");
+		double ave_time = timeList.stream().mapToLong(e -> e).average().getAsDouble();
+		double ave_length = pathLengthList.stream().mapToDouble(e -> e).average().getAsDouble();
+		double sd_time = Math.sqrt(timeList.stream().mapToDouble(e -> (e - ave_time) * (e - ave_time)).average().getAsDouble());
+		double sd_length = Math.sqrt(pathLengthList.stream().mapToDouble(e -> (e - ave_length) * (e - ave_length)).average().getAsDouble());
+		System.out.println("time:" + ave_time + "(" + sd_time +")");
+		System.out.println("length:" + ave_length + "(" + sd_length +")");
 	}
 
 }
