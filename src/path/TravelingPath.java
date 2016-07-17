1d7b7a6d31a78f5da9a1e56fadb8f2d65550b397
oden <iamoden41@gmail.com>
2016-07-18 00:06:23 +0900

    b:c間の分割，反転　完成？

--- src/path/TravelingPath.java
+++ src/path/TravelingPath.java
@@ -6,6 +6,7 @@ import data.CitiesData;
 
 public class TravelingPath {
     private int[] path;
+    private int[] segID;
     private final int noOfCities;
     private double tourLength;
 
@@ -24,6 +25,7 @@ public class TravelingPath {
 	tourLength = 0;
 	random = new Random();
 	path = new int[noOfCities];
+	segID = new int[(int) Math.sqrt(noOfCities)];
 
 	segmentIDFromCityID = new int[noOfCities];
 	indexInSegmentFromCityID = new int[noOfCities];
@@ -38,12 +40,15 @@ public class TravelingPath {
 	for (int i = 0; i < path.length; i++) {
 	    path[i] = i;
 	}
+	for (int i = 0; i < TSegment.length; i++) {
+	    segID[i] = i;
+	}
 	shuffle();
 
 	int count = 0;
 	for (int i = 0; i < TSegment.length; i++) {
-	    segIDFromSegIndex[i] = i;
-	    segIndexFromSegID[i] = i;
+	    segIDFromSegIndex[i] = segID[i];
+	    segIndexFromSegID[segID[i]] = i;
 
 	    if (i == TSegment.length - 1) {
 		TSegment[i] = new int[path.length - count];
@@ -131,70 +136,136 @@ public class TravelingPath {
     }
 
     public void filpPath(int a, int b, int c, int d, int i) {
-	// 交換範囲を決定
-	int start, end;
-	if (i == 0) {
-	    // a,b
-	    if (segIndexFromSegID[segmentIDFromCityID[a]] < segIndexFromSegID[segmentIDFromCityID[c]]
-		    || segmentIDFromCityID[a] == segmentIDFromCityID[c]
-			    && (indexInSegmentFromCityID[a] < indexInSegmentFromCityID[c]
-				    && RevFlagFromSegID[segmentIDFromCityID[a]] == false
-				    || indexInSegmentFromCityID[a] > indexInSegmentFromCityID[c]
-					    && RevFlagFromSegID[segmentIDFromCityID[a]] == true)) {
-		// a,b,c,d
-		start = b;
-		end = c;
+	if (segmentIDFromCityID[b] == segmentIDFromCityID[c]) {
+	    return;
+	} else if ((indexInSegmentFromCityID[b] == 0 && !RevFlagFromSegID[segmentIDFromCityID[b]]
+		|| indexInSegmentFromCityID[b] == TSegment[segmentIDFromCityID[b]].length - 1
+			&& RevFlagFromSegID[segmentIDFromCityID[b]])
+		&& (indexInSegmentFromCityID[c] == TSegment[segmentIDFromCityID[c]].length - 1
+			&& !RevFlagFromSegID[segmentIDFromCityID[c]]
+			|| indexInSegmentFromCityID[c] == 0 && RevFlagFromSegID[segmentIDFromCityID[c]])) {
+
+	    int startIndex = segIndexFromSegID[segmentIDFromCityID[b]];
+	    int endIndex = segIndexFromSegID[segmentIDFromCityID[c]];
+	    int swapCount = 0;
+	    if (startIndex == endIndex) {
+		RevFlagFromSegID[segIDFromSegIndex[startIndex]] = !RevFlagFromSegID[segIDFromSegIndex[startIndex]];
+		return;
+	    } else if (startIndex < endIndex) {
+		swapCount = (endIndex - startIndex + 1) / 2;
 	    } else {
-		// c,d,a,b
-		start = d;
-		end = a;
+		swapCount = (TSegment.length - (startIndex - 1) + endIndex) / 2;
 	    }
-	} else {
-	    // b,a
-	    if (segIndexFromSegID[segmentIDFromCityID[a]] < segIndexFromSegID[segmentIDFromCityID[c]]
-		    || segmentIDFromCityID[a] == segmentIDFromCityID[c]
-			    && (indexInSegmentFromCityID[a] < indexInSegmentFromCityID[c]
-				    && RevFlagFromSegID[segmentIDFromCityID[a]] == false
-				    || indexInSegmentFromCityID[a] > indexInSegmentFromCityID[c]
-					    && RevFlagFromSegID[segmentIDFromCityID[a]] == true)) {
-		// b,a,d,c
-		start = a;
-		end = d;
-	    } else {
-		// d,c,b,a
-		start = c;
-		end = b;
+	    int i1 = 0;
+	    while (i1 < swapCount) {
+		RevFlagFromSegID[segIDFromSegIndex[startIndex]] = !RevFlagFromSegID[segIDFromSegIndex[startIndex]];
+		RevFlagFromSegID[segIDFromSegIndex[endIndex]] = !RevFlagFromSegID[segIDFromSegIndex[endIndex]];
+		swapSegment(startIndex, endIndex);
+		i1++;
+		startIndex++;
+		if (startIndex == TSegment.length)
+		    startIndex = 0;
+		endIndex--;
+		if (endIndex == -1)
+		    endIndex = TSegment.length - 1;
 	    }
-	}
-
-	if (segmentIDFromCityID[start] == segmentIDFromCityID[end]) {
-
+	    if (startIndex == endIndex)
+		RevFlagFromSegID[segIDFromSegIndex[startIndex]] = !RevFlagFromSegID[segIDFromSegIndex[startIndex]];
 	} else {
-	    if ((indexInSegmentFromCityID[start] == 0 && !RevFlagFromSegID[segmentIDFromCityID[start]]
-		    || indexInSegmentFromCityID[start] == TSegment[segmentIDFromCityID[start]].length - 1
-			    && RevFlagFromSegID[segmentIDFromCityID[start]])
-		    && (indexInSegmentFromCityID[end] == TSegment[segmentIDFromCityID[end]].length - 1
-			    && !RevFlagFromSegID[segmentIDFromCityID[end]]
-			    || indexInSegmentFromCityID[end] == 0 && RevFlagFromSegID[segmentIDFromCityID[end]])) {
-		// 分割する必要ないパターン
-		int startIndex = segIndexFromSegID[segmentIDFromCityID[start]];
-		int endIndex = segIndexFromSegID[segmentIDFromCityID[end]];
-		while (startIndex < endIndex) {
-		    swapSegment(startIndex, endIndex);
-		    RevFlagFromSegID[segIDFromSegIndex[startIndex]] = !RevFlagFromSegID[segIDFromSegIndex[startIndex]];
-		    RevFlagFromSegID[segIDFromSegIndex[endIndex]] = !RevFlagFromSegID[segIDFromSegIndex[endIndex]];
-
-		    startIndex++;
-		    endIndex++;
+	    int startSplitSegIndex = segIndexFromSegID[segmentIDFromCityID[b]],
+		    endSplitSegIndex = segIndexFromSegID[segmentIDFromCityID[c]];
+	    int[] startSplit, endSplit;
+	    // 分割
+	    if (!(indexInSegmentFromCityID[b] == 0 && !RevFlagFromSegID[segmentIDFromCityID[b]]
+		    || indexInSegmentFromCityID[b] == TSegment[segmentIDFromCityID[b]].length - 1
+			    && RevFlagFromSegID[segmentIDFromCityID[b]])) {
+		int plot = indexInSegmentFromCityID[b];
+		int id = segmentIDFromCityID[b];
+		if (RevFlagFromSegID[segmentIDFromCityID[b]]) {
+		    startSplit = new int[TSegment[id].length - plot - 1];
+		    for (int j = TSegment[id].length - 1; j > plot; j--) {
+			startSplit[TSegment[id].length - j - 1] = TSegment[id][j];
+		    }
+		    int[] newSegment = new int[plot + 1];
+		    for (int k = 0; k < plot + 1; k++) {
+			newSegment[k] = TSegment[id][k];
+			indexInSegmentFromCityID[newSegment[k]] = k;
+		    }
+		    TSegment[id] = newSegment;
+		} else {
+		    startSplit = new int[plot];
+		    for (int j = 0; j < plot; j++) {
+			startSplit[j] = TSegment[id][j];
+		    }
+		    int[] newSegment = new int[TSegment[id].length - plot];
+		    for (int k = plot; k < TSegment[id].length; k++) {
+			newSegment[k - plot] = TSegment[id][k];
+			indexInSegmentFromCityID[newSegment[k - plot]] = k - plot;
+		    }
+		    TSegment[id] = newSegment;
 		}
-		if (startIndex == endIndex)
-		    RevFlagFromSegID[segIDFromSegIndex[startIndex]] = !RevFlagFromSegID[segIDFromSegIndex[startIndex]];
-
-		System.out.println();
-	    } else {
-		// 分割必要あり
+		int nop = 0;
 	    }
+	    if (!(indexInSegmentFromCityID[c] == TSegment[segmentIDFromCityID[c]].length - 1
+		    && !RevFlagFromSegID[segmentIDFromCityID[c]]
+		    || indexInSegmentFromCityID[c] == 0 && RevFlagFromSegID[segmentIDFromCityID[c]])) {
+		int plot = indexInSegmentFromCityID[c];
+		int id = segmentIDFromCityID[c];
+		if (RevFlagFromSegID[segmentIDFromCityID[c]]) {
+		    endSplit = new int[plot];
+		    for (int j = endSplit.length - 1; j >= 0; j--) {
+			endSplit[endSplit.length - j - 1] = TSegment[id][j];
+		    }
+		    int[] newSegment = new int[TSegment[id].length - endSplit.length];
+		    for (int k = 0; k < newSegment.length; k++) {
+			newSegment[k] = TSegment[id][plot + k];
+			indexInSegmentFromCityID[newSegment[k]] = k;
+		    }
+		    TSegment[id] = newSegment;
+		} else {
+		    endSplit = new int[TSegment[id].length - plot - 1];
+		    for (int j = plot + 1; j < TSegment[id].length; j++) {
+			endSplit[j - (plot + 1)] = TSegment[id][j];
+		    }
+		    int[] newSegment = new int[plot + 1];
+		    for (int k = 0; k < newSegment.length; k++) {
+			newSegment[k] = TSegment[id][k];
+			indexInSegmentFromCityID[newSegment[k]] = k;
+		    }
+		    TSegment[id] = newSegment;
+		}
+	    }
+
+	    // 反転
+	    int swapCount = 0;
+	    int temp1 = startSplitSegIndex;
+	    int temp2 = endSplitSegIndex;
+	    if (startSplitSegIndex < endSplitSegIndex) {
+		swapCount = (endSplitSegIndex - startSplitSegIndex + 1) / 2;
+	    } else {
+		swapCount = (TSegment.length - (startSplitSegIndex - 1) + endSplitSegIndex) / 2;
+	    }
+	    int i1 = 0;
+	    while (i1 < swapCount) {
+		RevFlagFromSegID[segIDFromSegIndex[startSplitSegIndex]] = !RevFlagFromSegID[segIDFromSegIndex[startSplitSegIndex]];
+		RevFlagFromSegID[segIDFromSegIndex[endSplitSegIndex]] = !RevFlagFromSegID[segIDFromSegIndex[endSplitSegIndex]];
+		swapSegment(startSplitSegIndex, endSplitSegIndex);
+		i1++;
+		startSplitSegIndex++;
+		if (startSplitSegIndex == TSegment.length)
+		    startSplitSegIndex = 0;
+		endSplitSegIndex--;
+		if (endSplitSegIndex == -1)
+		    endSplitSegIndex = TSegment.length - 1;
+	    }
+	    if (startSplitSegIndex == endSplitSegIndex)
+		RevFlagFromSegID[segIDFromSegIndex[startSplitSegIndex]] = !RevFlagFromSegID[segIDFromSegIndex[startSplitSegIndex]];
+
+	    // 結合
+	    startSplitSegIndex = temp1;
+	    endSplitSegIndex = temp2;
 	}
+
     }
 
     private void calcTourLength() {
@@ -232,15 +303,25 @@ public class TravelingPath {
     public final void shuffle() {
 	for (int i = 0; i < noOfCities; i++) {
 	    int index = random.nextInt(noOfCities - i) + i;
-	    swap(i, index);
+	    swapPath(i, index);
+	}
+	for (int i = 0; i < TSegment.length; i++) {
+	    int index = random.nextInt(TSegment.length - i) + i;
+	    swapSegID(i, index);
 	}
     }
 
-    public void swap(int index1, int index2) {
+    public void swapPath(int index1, int index2) {
 	int temp = path[index1];
 	int temp2 = path[index2];
 	path[index1] = temp2;
 	path[index2] = temp;
+    }
+
+    private void swapSegID(int index1, int index2) {
+	int temp = segID[index1];
+	segID[index1] = segID[index2];
+	segID[index2] = temp;
     }
 
     private final void swapSegment(int index1, int index2) {
@@ -256,13 +337,12 @@ public class TravelingPath {
 
     public static void main(String[] args) {
 	while (true) {
-	    TravelingPath path = new TravelingPath(20);
+	    TravelingPath path = new TravelingPath(50);
 	    path.init();
 	    path.RevFlagFromSegID[0] = true;
 	    path.RevFlagFromSegID[2] = true;
 	    Random random = new Random();
 	    path.filpPath(0, path.nextCity(0), 1, path.nextCity(1), 0);
-	    System.out.println();
 	}
     }
 }
